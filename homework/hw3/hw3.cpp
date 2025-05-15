89dd8842d0fed6fe9e0bc93e4f1b2eab47c799aa
Yazhou <yazhou.zhang@mail.utoronto.ca>
2025-05-14 20:31:14 -0700

    hw3

--- homework/hw3/hw3.cpp
+++ homework/hw3/hw3.cpp
@@ -5,6 +5,7 @@
 #include <mutex>
 #include <string>
 #include <thread>
+#include <fstream>
 
 // sai main libraries includes
 #include "SaiModel.h"
@@ -27,6 +28,37 @@ using namespace Eigen;
 
 // config file names and object names
 const string robot_file = "${CS225A_URDF_FOLDER}/panda/panda_arm.urdf";
+
+// Function to log joint trajectories
+void logJointAndTaskTrajectory(std::ofstream &log_file, double time, 
+                               const VectorXd &q, 
+                               const Vector3d &x, 
+                               const Vector3d &dx) {
+    log_file << time;
+
+    // joint positions
+    for (int i = 0; i < q.size(); ++i) {
+        log_file << "," << q(i);
+    }
+    // task positions
+    for (int i = 0; i < 3; ++i) {
+        log_file << "," << x(i);
+    }
+    // task velocities
+    for (int i = 0; i < 3; ++i) {
+        log_file << "," << dx(i);
+    }
+
+    log_file << std::endl;
+}
+
+double sat(double x) {
+    if (std::abs(x) <= 1.0) {
+        return x;
+    } else {
+        return (x > 0.0) ? 1.0 : -1.0;
+    }
+}
 
 int main(int argc, char** argv) {
     SaiModel::URDF_FOLDERS["CS225A_URDF_FOLDER"] = string(CS225A_URDF_FOLDER);
@@ -115,6 +147,47 @@ int main(int argc, char** argv) {
     const double control_freq = 1000;
     SaiCommon::LoopTimer timer(control_freq);
 
+    // Setup Log File
+    std::string filename = "joint_trajectory_q" + std::to_string(controller_number) + ".csv";
+    std::ofstream log_file(filename);
+    log_file << "time";
+    for (int i = 0; i < dof; ++i) {
+        log_file << ",q" << i + 1;
+    }
+    for (int i = 0; i < 3; ++i) {
+        log_file << ",x" << i + 1;
+    }
+    for (int i = 0; i < 3; ++i) {
+        log_file << ",dx" << i + 1;
+    }
+    log_file << std::endl;
+
+    std::ofstream torque_log_file;
+    std::ofstream delta_phi_log_file;
+    if (controller_number == 1) {
+        std::string torque_filename = "NT_torque_joint_q1.csv";
+        torque_log_file.open(torque_filename);
+        torque_log_file << "time";
+        for (int i = 0; i < dof; ++i) {
+            torque_log_file << ",NT_torque_joint_" << i + 1;
+        }
+        torque_log_file << std::endl;
+    }
+    else if (controller_number == 2) {
+        std::string torque_filename = "NT_torque_posture_q2.csv";
+        torque_log_file.open(torque_filename);
+        torque_log_file << "time";
+        for (int i = 0; i < dof; ++i) {
+            torque_log_file << ",NT_torque_posture_" << i + 1;
+        }
+        torque_log_file << std::endl;
+    }
+    else if (controller_number == 3) {
+        delta_phi_log_file.open("delta_phi_log.csv");
+        delta_phi_log_file << "time,delta_phi_x,delta_phi_y,delta_phi_z" << std::endl;
+    }
+
+
     while (runloop) {
         // wait for next scheduled loop
         timer.waitForNextLoop();
@@ -133,26 +206,234 @@ int main(int argc, char** argv) {
         // ---------------------------  question 1 ---------------------------------------
         if(controller_number == 1) {
 
-            control_torques.setZero();
+            double kp = 100.0;
+            double kv = 20.0;
+            double kpj = 50.0;
+            double kvj = 14.0;
+
+            Vector3d xd;
+            xd(0) = 0.3 + 0.1 * std::sin(M_PI * time);
+            xd(1) = 0.1 + 0.1 * std::cos(M_PI * time);
+            xd(2) = 0.5;
+
+            Vector3d dxd;
+            dxd(0) = 0.1 * M_PI * std::cos(M_PI * time);
+            dxd(1) = 0.1 * M_PI * -std::sin(M_PI * time);
+            dxd(2) = 0;
+
+            Vector3d ddxd;
+            ddxd(0) = 0.1 * M_PI * M_PI * -std::sin(M_PI * time);
+            ddxd(1) = 0.1 * M_PI * M_PI * -std::cos(M_PI * time);
+            ddxd(2) = 0;
+
+            VectorXd q_desired = VectorXd::Zero(dof);
+
+            // Current state
+            Vector3d x = robot->position(link_name, pos_in_link);             // EE pos
+            Vector3d dx = robot->linearVelocity(link_name, pos_in_link);      // EE vel
+
+            // Task-space control force
+            // Vector3d F = Lambda * (-kp * (x - xd) - kv * dx);                 // 3x1
+
+            // 1c improved trajectory tracking
+            Vector3d F = Lambda * (ddxd - kp * (x - xd) - kv * (dx - dxd));
+
+            // Map to joint torques
+            control_torques = Jv.transpose() * F;                                 // Nx1
+            control_torques += N.transpose() * (-kpj * (robot_q - q_desired) - kvj * robot_dq) + robot->jointGravityVector();
+
+            torque_log_file << time;
+            VectorXd NT_torque_joint(dof);
+            NT_torque_joint = N.transpose() * (-kpj * (robot_q - q_desired) - kvj * robot_dq);
+            for (int i = 0; i < dof; ++i) {
+                torque_log_file << "," << NT_torque_joint(i);
+            }
+            torque_log_file << std::endl;
+
         }
 
         // ---------------------------  question 2 ---------------------------------------
         else if(controller_number == 2) {
 
-            control_torques.setZero();
+            double kp = 100.0;
+            double kv = 20.0;
+            double kpj = 50.0;
+            double kvj = 14.0;
+
+            const double DEG2RAD = M_PI / 180.0;
+            VectorXd q_upper(dof);
+            q_upper << 165 * DEG2RAD, 100 * DEG2RAD, 165 * DEG2RAD, -30 * DEG2RAD, 165 * DEG2RAD, 210  * DEG2RAD, 165 * DEG2RAD;
+
+            VectorXd q_lower(dof);
+            q_lower << -165 * DEG2RAD, -100 * DEG2RAD, -165 * DEG2RAD, -170 * DEG2RAD, -165 * DEG2RAD, 0  * DEG2RAD, -165 * DEG2RAD;
+
+            double k_mid = 25.0;
+            VectorXd torque_mid(dof);
+            torque_mid = - 2.0 * k_mid * (robot_q - (q_upper + q_lower) / 2.0);
+
+            double k_damp = 14.0;
+            VectorXd torque_damp(dof);
+            torque_damp = - k_damp * robot_dq;
+
+            // Current state
+            Vector3d x = robot->position(link_name, pos_in_link);
+            Vector3d dx = robot->linearVelocity(link_name, pos_in_link);
+
+            ////////////////////////////////////////////
+            // // Problem 2b
+            // Vector3d xd;
+            // xd(0) = 0.3 + 0.1 * std::sin(M_PI * time);
+            // xd(1) = 0.1 + 0.1 * std::cos(M_PI * time);
+            // xd(2) = 0.5;
+
+            // // Task-space control force
+            // Vector3d F = Lambda * (-kp * (x - xd) - kv * dx);
+            // control_torques = Jv.transpose() * F;
+            // control_torques += N.transpose() * torque_mid + N.transpose() * torque_damp + robot->jointGravityVector();
+
+            // torque_log_file << time;
+            // VectorXd NT_torque_posture(dof);
+            // NT_torque_posture = N.transpose() * torque_mid + N.transpose() * torque_damp;
+            // for (int i = 0; i < dof; ++i) {
+            //     torque_log_file << "," << NT_torque_posture(i);
+            // }
+            // torque_log_file << std::endl;
+
+            //////////////////////////////////////////////
+            //  Problem 2d
+            Vector3d xd;
+            xd << -0.1, 0.15, 0.2;
+
+            // Task-space control force
+            Vector3d F = Lambda * (-kp * (x - xd) - kv * dx);
+            control_torques = Jv.transpose() * F;
+            control_torques += N.transpose() * torque_damp + robot->jointGravityVector();
+
+            //////////////////////////////////////////////
+            // // Problem 2e
+            // Vector3d xd;
+            // xd << -0.1, 0.15, 0.2;
+
+            // // Task-space control force
+            // Vector3d F = Lambda * (-kp * (x - xd) - kv * dx);
+            // control_torques = Jv.transpose() * F;
+            // control_torques += N.transpose() * torque_mid + N.transpose() * torque_damp + robot->jointGravityVector();
+
+            //////////////////////////////////////////////
+            // // Problem 2f
+            // Vector3d xd;
+            // xd << -0.65, -0.45, 0.7;
+
+            // // Task-space control force
+            // Vector3d F = Lambda * (-kp * (x - xd) - kv * dx);
+            // control_torques = Jv.transpose() * F;
+            // control_torques += N.transpose() * torque_mid + N.transpose() * torque_damp + robot->jointGravityVector();
+
+            ////////////////////////////////////////////
+            // // Problem 2g
+            // Vector3d xd;
+            // xd << -0.65, -0.45, 0.7;
+
+            // // Task-space control force
+            // Vector3d F = Lambda * (-kp * (x - xd) - kv * dx);
+            // control_torques = Jv.transpose() * F;
+            // control_torques += torque_mid + N.transpose() * torque_damp + robot->jointGravityVector();
+
+
         }
 
         // ---------------------------  question 3 ---------------------------------------
         else if(controller_number == 3) {
 
-            control_torques.setZero();
+            double kp = 50.0;
+            double kv = 25.0;
+            double kvj = 25.0;
+
+            // Current state
+            Vector3d x = robot->position(link_name, pos_in_link);
+            Vector3d dx = robot->linearVelocity(link_name, pos_in_link);
+            Matrix3d R = robot->rotation(link_name);
+            Vector3d omega = robot->angularVelocity(link_name);
+            MatrixXd J = robot->J(link_name, pos_in_link);
+
+            // desired rotation
+            double theta = M_PI / 3.0;
+            Matrix3d Rd;
+            Rd << 
+                std::cos(theta),    0,  std::sin(theta),
+                0,                  1,  0,
+                -std::sin(theta),   0,  std::cos(theta);
+
+            // desired position
+            Vector3d xd;
+            xd << 0.6, 0.3, 0.5;
+
+            // delta phi
+            Vector3d delta_phi = Vector3d::Zero();
+            for (int i=0; i<3; i++){
+                delta_phi += R.col(i).cross(Rd.col(i));
+            }
+            delta_phi  *= -0.5;
+
+            // F
+            VectorXd F(6);
+            F.head(3) = - kp * (x - xd) - kv * dx;
+            F.tail(3) = - kp * (delta_phi) - kv * omega;
+            F = Lambda * F;
+
+            // torque
+            control_torques = J.transpose() * F;
+            control_torques += - N.transpose() * kvj * robot_dq + robot->jointGravityVector();
+
+            // Log delta_phi
+            delta_phi_log_file << time << "," 
+                            << delta_phi(0) << "," 
+                            << delta_phi(1) << "," 
+                            << delta_phi(2) << std::endl;
+
+
+
         }
 
         // ---------------------------  question 4 ---------------------------------------
         else if(controller_number == 4) {
 
-            control_torques.setZero();
-            
+            double Vmax = 0.1;
+    
+            double kp = 200.0;
+            double kv = 32.0;
+            double kpj = 5.0;
+            double kvj = 30.0;
+
+            Vector3d xd(0.6, 0.3, 0.4);
+
+            const double DEG2RAD = M_PI / 180.0;
+            VectorXd q_upper(dof);
+            q_upper << 165 * DEG2RAD, 100 * DEG2RAD, 165 * DEG2RAD, -30 * DEG2RAD, 165 * DEG2RAD, 210  * DEG2RAD, 165 * DEG2RAD;
+            VectorXd q_lower(dof);
+            q_lower << -165 * DEG2RAD, -100 * DEG2RAD, -165 * DEG2RAD, -170 * DEG2RAD, -165 * DEG2RAD, 0  * DEG2RAD, -165 * DEG2RAD;
+            VectorXd q_desired = (q_upper + q_lower)/2;
+
+            // Current state
+            Vector3d x = robot->position(link_name, pos_in_link);
+            Vector3d dx = robot->linearVelocity(link_name, pos_in_link);
+
+            ////////////////////////////////////////////
+            // Problem 4a
+            // Task-space control force
+            Vector3d F = Lambda * (-kp * (x - xd) - kv * dx);
+            control_torques = Jv.transpose() * F;
+            control_torques += N.transpose() * robot->M() * (-kpj * (robot_q - q_desired) - kvj * robot_dq) + robot->jointGravityVector();
+
+            ////////////////////////////////////////////
+            // // Problem 4b with velocity saturation
+            // Vector3d dxd = - (kp / kv) * (x - xd);
+            // double Vxd = dxd.norm();
+            // double nu = sat(Vmax/std::abs(Vxd));
+            // Vector3d F = Lambda * (-kv * (dx - nu * dxd));
+            // control_torques = Jv.transpose() * F;
+            // control_torques += N.transpose() * robot->M() * (-kpj * (robot_q - q_desired) - kvj * robot_dq) + robot->jointGravityVector();
+
         }
 
         // **********************
@@ -161,6 +442,11 @@ int main(int argc, char** argv) {
 
         // send to redis
         redis_client.sendAllFromGroup();
+
+        // Current state
+        Vector3d x = robot->position(link_name, pos_in_link);             // EE pos
+        Vector3d dx = robot->linearVelocity(link_name, pos_in_link);      // EE vel
+        logJointAndTaskTrajectory(log_file, time, robot_q, x, dx);
     }
 
     control_torques.setZero();
